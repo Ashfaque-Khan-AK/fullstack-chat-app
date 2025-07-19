ffa347daf9d0026c1310c81f405cbc0135a607fc
Ashfaque-Ahmed47 <ishfaque7878@gmail.com>
2025-07-19 18:00:52 +0500

    fixed profilePic typo error in the image chat

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -52,8 +52,8 @@ const ChatContainer = () => {
             <div className="chat-image avatar">
               <div className="size-10 rounded-full border">
                   <img src
-                  ={message.senderId === authUser._id ? authUser.prfofilePic || "/avatar.png" 
-                  : selectedUser.prfofilePic || "/avatar.png"
+                  ={message.senderId === authUser._id ? authUser.profilePic || "/avatar.png" 
+                  : selectedUser.profilePic || "/avatar.png"
                   }
                   alt="profile pic" 
                   />
