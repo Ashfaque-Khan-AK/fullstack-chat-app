7b8ca9c200d2094e83b5b5c2551935ff126017c0
Ashfaque-Ahmed47 <ishfaque7878@gmail.com>
2025-07-19 17:17:53 +0500

    fixed the typo error in JWT_SECRET as jWT_SECRET

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -8,7 +8,7 @@ export const protectRoute = async (req, res, next)=>{
     if (!token) {
         return res.status(401).json({message: "Unauthorized - No Token Provided"});
     }
-    const decoded = jwt.verify(token, process.env.jWT_SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     
     if (!decoded) {
         return res.status(401).json({message: "Unauthorized - Invlalid Token"});
